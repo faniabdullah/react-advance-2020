c4098f3d8faedc6057b1a6104dda357c1412f2c3
faniabdullah <tugasfani@gmail.com>
2020-12-22 22:23:00 +0700

    useState Array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ExampleComponent from './tutorial/1-useState/setup/2-useState-basics'
+import ExampleComponent from './tutorial/1-useState/setup/3-useState-array'
 function App() {
   return (
     <div className='container'>
