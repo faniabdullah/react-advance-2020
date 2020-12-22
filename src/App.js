b4bba988e340d8a1102e7e9f1b06f1278fe2f82a
faniabdullah <tugasfani@gmail.com>
2020-12-22 22:22:21 +0700

    useState Basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import ErrorExample from './tutorial/1-useState/setup/1-error-example'
+import ExampleComponent from './tutorial/1-useState/setup/2-useState-basics'
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
-      <ErrorExample/>
+      <ExampleComponent/>
     </div>
   )
 }
